4ea4f9267ce1c0ca9749017d98c63f38c897a6d9
Scott Lenander <sjlteacher@gmail.com>
2017-07-28 10:42:43 -0400

    Class Overview view added

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {registrations: 'registrations'}
 
   get 'about' => 'welcome#about'
-  
+
   authenticated :user do
     root 'welcome#dashboard', as: :authenticate_root
+    resources :classes
   end
 
   root 'welcome#index'
