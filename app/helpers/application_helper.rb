9202946975606f3e1759b802d6212b773e906a02
Scott Lenander <sjlteacher@gmail.com>
2017-08-11 15:57:57 -0400

    Users can view averaged grades in Student's Dashboard and Course Overview pages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,15 @@
 module ApplicationHelper
+
   def gravatar_url(email, size)
     gravatar = Digest::MD5::hexdigest(email).downcase
     url = "http://gravatar.com/avatar/#{gravatar}.png?s=#{size}&d=retro"
+  end
+
+  def avg_participation(grades)
+    ((grades.where(participation: 0).count * 100) + (grades.where(participation: 1).count * 50)) / grades.count(:participation)
+  end
+
+  def avg_homework(grades)
+    ((grades.where(homework: 0).count * 100) + (grades.where(homework: 1).count * 50)) / grades.count(:homework)
   end
 end
