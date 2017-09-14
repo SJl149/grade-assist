52cc69e0cff04712ba3ff22ba778e2e0bb8e6d26
Scott Lenander <sjlteacher@gmail.com>
2017-09-13 20:53:32 -0400

    Schema rearranged but untested

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,6 +5,8 @@ class Course < ActiveRecord::Base
   has_many :students, through: :enrollments
 
   validates :name, uniqueness: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 
   accepts_nested_attributes_for :enrollments, allow_destroy: true
   accepts_nested_attributes_for :students, allow_destroy: true
