8d2a68d092ce6266d119e4cef273c6660cefd7fd
ScottL <sjlteacher@gmail.com>
2017-09-20 13:04:39 -0400

    Comment out daily_grade_count.

--- db/migrate/20170821214221_remove_daily_grades_count_from_students.rb
+++ db/migrate/20170821214221_remove_daily_grades_count_from_students.rb
@@ -1,6 +1,6 @@
 class RemoveDailyGradesCountFromStudents < ActiveRecord::Migration
   def up
-    remove_column :students, :daily_grades_count
+    #remove_column :students, :daily_grades_count
   end
   def down
     add_column :students, :daily_grades_count, :integer, default: 0
