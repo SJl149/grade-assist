143b881abad382cc5770eb232e7728d5a5d61d2f
ScottL <sjlteacher@gmail.com>
2017-09-20 13:00:56 -0400

    Edit migrations.

--- db/migrate/20170728130347_create_esl_classes.rb
+++ db/migrate/20170728130347_create_esl_classes.rb
@@ -3,8 +3,8 @@ class CreateEslClasses < ActiveRecord::Migration
     create_table :esl_classes do |t|
       t.string :name
       t.belongs_to :user, index: true, foreign_key: true
-      t.belongs_to :student, index: true, foreign_key: true
-      
+      # t.belongs_to :student, index: true, foreign_key: true
+
       t.timestamps null: false
     end
   end
--- db/migrate/20170728152346_remove_student_id_from_esl_class.rb
+++ db/migrate/20170728152346_remove_student_id_from_esl_class.rb
@@ -1,6 +1,6 @@
 class RemoveStudentIdFromEslClass < ActiveRecord::Migration
   def up
-    remove_column :esl_classes, :student_id
+    # remove_column :esl_classes, :student_id
   end
   def down
     add_column :esl_classes, :student_id, :integer
