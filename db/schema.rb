deeb9bb4a44f82332ab77d6b4aa6d50554cc8948
ScottL <sjlteacher@gmail.com>
2017-09-28 17:36:11 -0400

    Drop Holidays table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920172706) do
+ActiveRecord::Schema.define(version: 20170928212901) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -58,13 +58,6 @@ ActiveRecord::Schema.define(version: 20170920172706) do
   end
 
   add_index "events", ["user_id"], name: "index_events_on_user_id"
-
-  create_table "holidays", force: :cascade do |t|
-    t.datetime "period"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "name"
-  end
 
   create_table "semesters", force: :cascade do |t|
     t.string   "name"
